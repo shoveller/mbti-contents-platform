d668289d74f64de0cfa52a88c1d87e16ee9b8212
cinos81 <cinos81@gmail.com>
2025-03-01 22:34:15 +0900

    feat: 리액트 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="flex flex-col items-center p-7 rounded-2xl">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img className="size-48 shadow-xl rounded-md" alt="" src="vite.svg" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex">
+        <span className="text-2xl font-medium">Class Warfare</span>
+        <span>The Anti-Patterns</span>
+        <span className="flex">
+          <span>No. 4</span>
+          <span>·</span>
+          <span>2025</span>
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
