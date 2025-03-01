86dc1e4e37754225f9f572baf48e7582746758fc
cinos81 <cinos81@gmail.com>
2025-03-01 23:06:19 +0900

    feat: 프로젝트 루트 기준 절대경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Outlet />}>
+      <Route index lazy={() => import("@/page/Main.tsx")} />
+    </Route>,
+  ),
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
---
+++ src/page/Main.tsx
@@ -0,0 +1,6 @@
+const Main = () => {
+  return <h1>섬네일 리스트 페이지</h1>;
+};
+
+export const Component = Main;
+export default Main;
