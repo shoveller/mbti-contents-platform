7feaf8a80315637c59cfcc42d324df9be9bf031c
cinos81 <cinos81@gmail.com>
2025-03-10 23:14:17 +0900

    feat: 원격 이미지를 로컬 이미지로 전환

--- src/data/TESTS.ts
+++ src/data/TESTS.ts
@@ -59,9 +59,9 @@ export const TESTS: TestData[] = [
       mainUrl: "personalColor",
       scoreType: "MBTI",
       mainImage:
-        "https://www.dropbox.com/scl/fi/u4moi8xe7wme1ao796ujw/personalColor-intro.png?rlkey=qn2igicxafksekjx9fsnukk9z&st=ifpx4r50&dl=1",
+        "/images/ko/personalColor-intro.png",
       thumbImage:
-        "https://www.dropbox.com/scl/fi/9iqe0gypnnclo5elt3hpi/personalColor-thumb.png?rlkey=m9uwdd6xww0v66g2gjo2688jz&st=ut5c22fv&dl=1",
+        "/images/ko/personalColor-thumb.png",
       lang: "ko",
       category: "characteristic",
     },
@@ -244,97 +244,97 @@ export const TESTS: TestData[] = [
         type: "ESTJ",
         query: "ESTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/5lhbern3h5b8w7ui6sssu/ESTJ.png?rlkey=bjudaghu8hm4d5ohgo24v3mbj&st=uy87vrg6&dl=1",
+          "/images/ko/ESTJ.png",
       },
       {
         type: "ESTP",
         query: "ESTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/8ytg6oypu645x0qpcfemt/ESTP.png?rlkey=in97p7velqy191srwufft1vs1&st=1pkilt92&dl=1",
+          "/images/ko/ESTP.png",
       },
       {
         type: "ESFJ",
         query: "ESFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/d91sjo51emt7y3stj7hbg/ESFJ.png?rlkey=xgxmki7qiwn4i0hdwj3vuhzsi&st=ethsiyxr&dl=1",
+          "/images/ko/ESFJ.png",
       },
       {
         type: "ESFP",
         query: "ESFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/r0vzqjgrqo0wj3u71uhk4/ESFP.png?rlkey=jadq00urhuwy7y9ag15o87p16&st=wdrn9n3d&dl=1",
+          "/images/ko/ESFP.png",
       },
       {
         type: "ENTJ",
         query: "ENTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/22icyye1317a1w1397z8q/ENTJ.png?rlkey=kavn4ijsa6q08adtwab18ylnj&st=icaonefg&dl=1",
+          "/images/ko/ENTJ.png",
       },
       {
         type: "ENTP",
         query: "ENTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/v05pict18syeudgdpzawc/ENTP.png?rlkey=7sj9z64pz5498hx6dpkpugpj0&st=kdcy2vwz&dl=1",
+          "/images/ko/ENTP.png",
       },
       {
         type: "ENFJ",
         query: "ENFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/e7m4ja909vw52jonqz2p3/ENFJ.png?rlkey=boxstq35qpqvre21aahwtxjif&st=9f4mv24e&dl=1",
+          "/images/ko/ENFJ.png",
       },
       {
         type: "ENFP",
         query: "ENFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/84up1wzeh3o92h3ndyyhy/ENFP.png?rlkey=az1j3kjxbbeqfxgc2ieb1e4uu&st=kcacavzx&dl=1",
+          "/images/ko/ENFP.png",
       },
       {
         type: "ISTJ",
         query: "ISTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/6nm4iwpja8ckf013l1fxm/ISTJ.png?rlkey=7hnj0pxcaeeq1nkmvlkjq0ihj&st=0dkq6mrp&dl=1",
+          "/images/ko/ISTJ.png",
       },
       {
         type: "ISTP",
         query: "ISTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/0xcrdzoe86vwug5xr16i0/ISTP.png?rlkey=mxow3zwad1vw74j1bqw0obvk7&st=w8baagct&dl=1",
+          "/images/ko/ISTP.png",
       },
       {
         type: "ISFJ",
         query: "ISFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/wtifdrfjb57kcarf5qk9m/ISFJ.png?rlkey=w1p61ver9kth4pfesbbda2u12&st=xm0f3xq1&dl=1",
+          "/images/ko/ISFJ.png",
       },
       {
         type: "ISFP",
         query: "ISFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/aj8oj1er6b3ouwisn5uk2/ISFP.png?rlkey=v9okq1di9twtwxnw9ln3t160x&st=pr0zx4w6&dl=1",
+          "/images/ko/ISFP.png",
       },
       {
         type: "INTJ",
         query: "INTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/ms3hxuc03g2ye9cl71ec1/INTJ.png?rlkey=nuadpr0hv3yfglfqerog5cejw&st=gao0ony6&dl=1",
+          "/images/ko/INTJ.png",
       },
       {
         type: "INTP",
         query: "INTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/to767xowr0fdwsp6896hv/INTP.png?rlkey=ehr8lpihjmsiepxlp5kdcxkel&st=38hrud5l&dl=1",
+          "/images/ko/INTP.png",
       },
       {
         type: "INFJ",
         query: "INFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/ak707z5pnsc2ng36z5s93/INFJ.png?rlkey=dm6qvh8bzdho49bikhi3msp45&st=ksw315sz&dl=1",
+          "/images/ko/INFJ.png",
       },
       {
         type: "INFP",
         query: "INFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/ap8zax8x2g1zabq7ydhze/INFP.png?rlkey=ez9gqtmjkgo1j8ko3shgdihqn&st=gvd5a7xd&dl=1",
+          "/images/ko/INFP.png",
       },
     ],
   },
@@ -353,9 +353,9 @@ export const TESTS: TestData[] = [
       mainUrl: "personalColorEng",
       scoreType: "MBTI",
       mainImage:
-        "https://www.dropbox.com/scl/fi/jmihoko6rjpk0d3rgewsr/personalColorEng-intro.png?rlkey=ifyyqa8m4w44vq1nk08fshhnj&st=n42rl4fc&dl=1",
+        "/images/en/personalColorEng-intro.png",
       thumbImage:
-        "https://www.dropbox.com/scl/fi/x2b3tms9ml6kbz3vcot9h/personalColorEng-thumb.png?rlkey=alaxfwbswgfn34xao2zm99a84&st=zvw3w510&dl=1",
+        "/images/en/personalColorEng-thumb.png",
       lang: "en",
       category: "characteristic",
     },
@@ -534,97 +534,97 @@ export const TESTS: TestData[] = [
         type: "ESTJ",
         query: "ESTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/7jyylkbnew8w67sadxwf3/ESTJ.png?rlkey=x28ejf7fphxcknxgrox4jt0xi&st=gi6jplvf&dl=1",
+          "/images/en/ESTJ.png",
       },
       {
         type: "ESTP",
         query: "ESTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/69zj9tuprhhhawqh3vb3p/ESTP.png?rlkey=3qzxrw45bbweszae5ujug6jrf&st=fp9olors&dl=1",
+          "/images/en/ESTP.png",
       },
       {
         type: "ESFJ",
         query: "ESFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/d91sjo51emt7y3stj7hbg/ESFJ.png?rlkey=xgxmki7qiwn4i0hdwj3vuhzsi&st=ethsiyxr&dl=1",
+          "/images/en/ESFJ.png",
       },
       {
         type: "ESFP",
         query: "ESFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/4en5qkoku1gdhqcy94gbn/ESFJ.png?rlkey=yysnrrp2292gb6v933lrn8od9&st=xzvb06i0&dl=1",
+          "/images/en/ESFJ.png",
       },
       {
         type: "ENTJ",
         query: "ENTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/d6yk934zbz3skyuzqx2uh/ENTJ.png?rlkey=ww8y9oig2cjvgedolhlzlydc4&st=zkhwom3a&dl=1",
+          "/images/en/ENTJ.png",
       },
       {
         type: "ENTP",
         query: "ENTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/nocstuf0kek8vqwof04kr/ENTP.png?rlkey=jhzncfwwge0vm0prc9z1q1us8&st=3nbx8feg&dl=1",
+          "/images/en/ENTP.png",
       },
       {
         type: "ENFJ",
         query: "ENFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/mz9xj0eiun4y0wyk6i68b/ENFJ.png?rlkey=a27hkov12q2jnh5afjpcb4vf8&st=0pwukbac&dl=1",
+          "/images/en/ENFJ.png",
       },
       {
         type: "ENFP",
         query: "ENFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/ytgc86i5mxjiynwrm4mfw/ENFP.png?rlkey=tomxuobmmpl9fma9ng67pw2d8&st=o0m2tssj&dl=1",
+          "/images/en/ENFP.png",
       },
       {
         type: "ISTJ",
         query: "ISTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/0uwl1mzly1mwi0fsu0acz/ISTJ.png?rlkey=nnbk0rn8w7di6iw7ipybpwwey&st=ra2mod39&dl=1",
+          "/images/en/ISTJ.png",
       },
       {
         type: "ISTP",
         query: "ISTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/6clhx19z1c0l04ulxf5y2/ISTP.png?rlkey=4fwnw6cebeekdvgq3i2hlgsl1&st=k789fxan&dl=1",
+          "/images/en/ISTP.png",
       },
       {
         type: "ISFJ",
         query: "ISFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/nrhs5u9dgk6w1gs90wkvv/ISFJ.png?rlkey=jxgftwx5hwg088zsbutxbqlgq&st=bi2ywew2&dl=1",
+          "/images/en/ISFJ.png",
       },
       {
         type: "ISFP",
         query: "ISFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/wdnx56kbkdal04nn6364w/ISFP.png?rlkey=5jyfp2x3dcsasjdqraviuqzcg&st=0g9zrn2z&dl=1",
+          "/images/en/ISFP.png",
       },
       {
         type: "INTJ",
         query: "INTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/hti638rqyby82gguj01z8/INTJ.png?rlkey=ijtbi4hh45dysynn3h6q3k9rg&st=trmrqhry&dl=1",
+          "/images/en/INTJ.png",
       },
       {
         type: "INTP",
         query: "INTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/nbena5wf4wuok93tftaxy/INTP.png?rlkey=q5d5oyzmrs8ytuiyzcbekm9v7&st=ev85otx2&dl=1",
+          "/images/en/INTP.png",
       },
       {
         type: "INFJ",
         query: "INFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/6wxifksbt0c936xlmo3d7/INFJ.png?rlkey=7l6o8yl266k4aor8kbru0b1hy&st=l6xxjeqc&dl=1",
+          "/images/en/INFJ.png",
       },
       {
         type: "INFP",
         query: "INFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/q4bpun6h0ddy1tf7a4h7c/INFP.png?rlkey=57mzaw4neohebbeh0xnrpcow9&st=m92y77uq&dl=1",
+          "/images/en/INFP.png",
       },
     ],
   },
@@ -643,9 +643,9 @@ export const TESTS: TestData[] = [
       mainUrl: "personalColorJP",
       scoreType: "MBTI",
       mainImage:
-        "https://www.dropbox.com/scl/fi/oyaueh1pzggp5s92532ba/personalColorJP-intro.png?rlkey=pc8j3xs4o64hlxoiqegk6nrdb&st=ipxjb532&dl=1",
+        "/images/jp/personalColorJP-intro.png",
       thumbImage:
-        "https://www.dropbox.com/scl/fi/kt272mdx3kigrtgwojeod/personalColorJP-thumb.png?rlkey=j19yzp6gt6iokqgg8kzrl1gpj&st=gzznyj10&dl=1",
+        "/images/jp/personalColorJP-thumb.png",
       lang: "jp",
       category: "characteristic",
     },
@@ -824,97 +824,97 @@ export const TESTS: TestData[] = [
         type: "ESTJ",
         query: "ESTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/pw32b9gizven8wqpw5dwf/ESTJ.png?rlkey=qtklzibgln378c3bdknwjw7tf&st=8l16g8ct&dl=1",
+          "/images/jp/ESTJ.png",
       },
       {
         type: "ESTP",
         query: "ESTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/ibgrksb70x2bjzcp0z04h/ESTP.png?rlkey=ik3lcdangfzm98erz6br44qb1&st=lhd9w11r&dl=1",
+          "/images/jp/ESTP.png",
       },
       {
         type: "ESFJ",
         query: "ESFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/3sj74dcaopwcef1nu21s5/ESFJ.png?rlkey=dsbzcq1d82u89qtzgvmke5ase&st=9cf15gjq&dl=1",
+          "/images/jp/ESFJ.png",
       },
       {
         type: "ESFP",
         query: "ESFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/fpewgwyldewu32atxl46r/ESFP.png?rlkey=wqk84mirvwsztbndmcxnyf5nq&st=6e0egn3c&dl=1",
+          "/images/jp/ESFP.png",
       },
       {
         type: "ENTJ",
         query: "ENTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/5z99j9pguw2v481c2684d/ENTJ.png?rlkey=gh85ty07z8wt487w67qxax5px&st=ss1r7dxx&dl=1",
+          "/images/jp/ENTJ.png",
       },
       {
         type: "ENTP",
         query: "ENTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/wbt2slrxassf1w1xkmtre/ENTP.png?rlkey=otdc1hazlyxnangfcryydh5k1&st=69k3sp7u&dl=1",
+          "/images/jp/ENTP.png",
       },
       {
         type: "ENFJ",
         query: "ENFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/45rtpzm7yc0abt0jpw4dg/ENFJ.png?rlkey=03bckb05d1vkz534jkqskdunh&st=sja06fnq&dl=1",
+          "/images/jp/ENFJ.png",
       },
       {
         type: "ENFP",
         query: "ENFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/q6mzyi55sr85bpaahvrbf/ENFP.png?rlkey=3721niechpy0xsvvc2i9rzrd3&st=96zbdbsu&dl=1",
+          "/images/jp/ENFP.png",
       },
       {
         type: "ISTJ",
         query: "ISTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/65z4aoimkhjya6wmu7vo8/ISTJ.png?rlkey=guf8653rlsy12qwqju2prox59&st=fgwc8cnp&dl=1",
+          "/images/jp/ISTJ.png",
       },
       {
         type: "ISTP",
         query: "ISTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/be1p8s7v4y3cn5l5jqm1q/ISTP.png?rlkey=3zylu48zhmqmit9ihfy76487u&st=8vdfflut&dl=1",
+          "/images/jp/ISTP.png",
       },
       {
         type: "ISFJ",
         query: "ISFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/kqlmkbn2pqvmf34ezybcd/ISFJ.png?rlkey=sd3dryf9pahqaq83t23g5hiq8&st=kxo5opty&dl=1",
+          "/images/jp/ISFJ.png",
       },
       {
         type: "ISFP",
         query: "ISFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/0p3eidtg3jcmfmxp6f59s/ISFP.png?rlkey=wmktoly5dyl6xezae7nwxwu3u&st=sh6o7h2i&dl=1",
+          "/images/jp/ISFP.png",
       },
       {
         type: "INTJ",
         query: "INTJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/ibyezz790rxzciz7mmdmb/INTJ.png?rlkey=aewbj0ydimf4z1xv0olhpc2zh&st=lcotd17i&dl=1",
+          "/images/jp/INTJ.png",
       },
       {
         type: "INTP",
         query: "INTP",
         img_src:
-          "https://www.dropbox.com/scl/fi/gnthsbrbmpe23m07dacyt/INTP.png?rlkey=i45gddj68d5go2mpanknj4yf7&st=aois0s0l&dl=1",
+          "/images/jp/INTP.png",
       },
       {
         type: "INFJ",
         query: "INFJ",
         img_src:
-          "https://www.dropbox.com/scl/fi/tndrtsb971747ngvvhh8a/INFJ.png?rlkey=y7lvzvphffjam2yr5hpsqhbnz&st=1sk1yal6&dl=1",
+          "/images/jp/INFJ.png",
       },
       {
         type: "INFP",
         query: "INFP",
         img_src:
-          "https://www.dropbox.com/scl/fi/8r17nrwcdlbop2nluv6c3/INFP.png?rlkey=6atqeqic5a98adqima7ii56qm&st=pf2ucdmw&dl=1",
+          "/images/jp/INFP.png",
       },
     ],
   },
