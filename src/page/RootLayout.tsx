82e3168a5b0116c448f26efc5c1c77c34c883a36
cinos81 <cinos81@gmail.com>
2025-03-11 23:15:18 +0900

    fix: 잠재된 버그를 수정

--- src/page/RootLayout.tsx
+++ src/page/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div className="w-full max-w-[35rem] mx-auto px-[5px] text-center m-8">
+    <div className="w-full max-w-[35rem] mx-auto px-[5px] text-center my-8">
       <Outlet />
     </div>
   );
