8d0d5e31460d5865334e96d1723ff79c2462a841
cinos81 <cinos81@gmail.com>
2025-03-23 00:11:30 +0900

    feat: eslint.config.js 삭제 및 eslint.config.mjs로 통합, 관련 문서 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,88 @@
- 
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+const defaultCodeStyle = {
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: {
+      ...globals.browser,
+      ...globals.node
+    }
+  },
+  plugins: {
+    'unused-imports': unusedImports
+  },
+  rules: {
+    'max-depth': ['error', 2],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'if' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'function' }
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSInterfaceDeclaration',
+        message: 'Interface 대신 type 을 사용하세요.'
+      },
+      {
+        selector: 'VariableDeclaration[kind="let"]',
+        message: 'let 대신 const 를 사용하세요.'
+      },
+      {
+        selector: 'VariableDeclaration[kind="var"]',
+        message: 'var 대신 const 를 사용하세요.'
+      },
+      {
+        selector: 'SwitchStatement',
+        message: 'switch 대신 if 를 사용하세요.'
+      },
+      {
+        selector: 'ConditionalExpression',
+        message: '삼항 연산자 대신 if 를 사용하세요.'
+      },
+      {
+        selector: 'IfStatement[alternate]',
+        message: 'else 대신 early return 을 사용하세요.'
+      }
+    ],
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}
+
+export default tseslint.config({ ignores: ['dist'] }, defaultCodeStyle, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ]
+  }
+})
